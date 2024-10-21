823172aa696581383400284f3b31bddbeb102d95
Felipe Cerqueira <felipecer06@gmail.com>
2024-10-21 17:19:49 -0300

    finalização tarefa

--- asyncviews/urls.py
+++ asyncviews/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', views.async_view),
+    path('', views.async_view),
     path('sync/', views.sync_view)
 ]
